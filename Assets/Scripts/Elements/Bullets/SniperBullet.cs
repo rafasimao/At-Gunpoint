3014609d1cd0aa178a061f09b9f0b10c6e62a9e5
rafael simao <raficher@gmail.com>
2015-11-11 18:59:18 -0200

    SniperBullet

---
+++ Assets/Scripts/Elements/Bullets/SniperBullet.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SniperBullet : Bullet
+{
+	public float MinVelocity;
+	Rigidbody _Rigidbody;
+	
+	void Awake ()
+	{
+		_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void OnEnable ()
+	{
+		Invoke("Deactivate", 2f);
+		_Rigidbody.AddForce(Direction*Speed,ForceMode.Impulse);
+	}
+	
+	void OnCollisionEnter (Collision collision) 
+	{
+		Bullet other = collision.gameObject.GetComponent<Bullet>();
+		if (other == null)
+		{
+			Damageable obj = collision.gameObject.GetComponent<Damageable>();
+			
+			if (obj != null)
+				obj.TakeDamage(Damage);
+
+			Debug.Log(_Rigidbody.velocity.magnitude);
+			if (_Rigidbody.velocity.magnitude < MinVelocity)
+				gameObject.SetActive(false);
+		}
+	}
+	
+	void Deactivate ()
+	{
+		gameObject.SetActive(false);
+	}
+	
+	void OnDisable ()
+	{
+		CancelInvoke();
+	}
+}
