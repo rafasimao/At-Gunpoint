e7335326ad0ba80dceded8704952bb5b455aab8c
rafael simao <raficher@gmail.com>
2015-11-11 09:52:15 -0200

    Created Rotator and Applied to Coin

---
+++ Assets/Scripts/General/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour 
+{
+	public Vector3 RotationVector;
+	public float Speed;
+
+	void Update () 
+	{
+		transform.Rotate (RotationVector * Speed * Time.deltaTime);
+	}
+}
