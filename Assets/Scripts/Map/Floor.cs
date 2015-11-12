09bfca30acab9f498a2e237f704452198f6644c2
rafael simao <raficher@gmail.com>
2015-11-12 11:40:06 -0200

    Fixed pass more than once through one floor bug

--- Assets/Scripts/Map/Floor.cs
+++ Assets/Scripts/Map/Floor.cs
@@ -6,11 +6,14 @@ public class Floor : MonoBehaviour
 
 	public MapController Map;
 
+	bool _Triggered;
+
 	void OnTriggerEnter (Collider other) 
 	{
-		if (other.tag.Equals("Player"))
+		if (!_Triggered && other.tag.Equals("Player"))
 		{
 			Map.OnFloorTriggered(this);
+			_Triggered = true;
 		}
 	}
 
@@ -18,5 +21,7 @@ public class Floor : MonoBehaviour
 	{
 		Vector3 pos = transform.position;
 		transform.position = new Vector3(pos.x+offset, pos.y, pos.z);
+		// Reload trigger
+		_Triggered = false;
 	}
 }
