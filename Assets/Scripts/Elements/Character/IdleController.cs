20ddc86aaa8d01323decc54c315a2d6032b4711f
rafael simao <raficher@gmail.com>
2015-11-12 11:34:52 -0200

    Fixed Idle Animation

---
+++ Assets/Scripts/Elements/Character/IdleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleController : MonoBehaviour
+{
+	public Animator CharacterAnimator;
+	public int NumberOfIdles;
+	public float Delay;
+
+	float _Timer;
+
+	void Update ()
+	{
+		_Timer += Time.deltaTime;
+		if (_Timer > Delay)
+		{
+			ChangeIdle();
+			_Timer = 0f;
+		}
+	}
+
+	void ChangeIdle ()
+	{
+		CharacterAnimator.SetTrigger("ChangeIdle");
+		CharacterAnimator.SetInteger("IdleType",Random.Range(0,NumberOfIdles));
+	}
+
+}
