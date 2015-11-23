df10377fa0e7cb5434a12f094148c4f7afd5a102
rafael simao <raficher@gmail.com>
2015-11-23 15:08:34 -0200

    Created FXPlayerDistanceTrigger
    Created a script to trigger effects from the FXController when the
    player reach a certain distance from the object

---
+++ Assets/Scripts/General/Components/FXPlayerDistanceTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FXPlayerDistanceTrigger : MonoBehaviour 
+{
+	public float TriggerDistance = 15f;
+	public FXController.FXTypes FXType;
+
+	Transform _Player;
+	bool _Played;
+
+	void OnEnable ()
+	{
+		_Played = false;
+		_Player = GameController.Instance.GamePlayer.SelectedChar.transform;
+	}
+
+	void Update()
+	{
+		if (!_Played && (_Player.position-transform.position).magnitude < TriggerDistance)
+		{
+			GameController.Instance.GameFXController.Play(FXType, transform.position);
+			_Played = true;
+			gameObject.SetActive(false);
+		}
+	}
+}
