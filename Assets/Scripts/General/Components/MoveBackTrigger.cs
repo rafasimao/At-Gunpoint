3b5a2937e4ff57ccebf61174070d667f99ec5af5
rafael simao <raficher@gmail.com>
2015-11-23 15:07:07 -0200

    Created MoveBackTrigger
    Created the script to use to redirect the player when it tries to go to
    a lane already occupied by something untresspassable

---
+++ Assets/Scripts/General/Components/MoveBackTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBackTrigger : MonoBehaviour 
+{
+	public enum Type {MoveBackUp, MoveBackDown};
+
+	public Type MoveBack;
+
+	void OnTriggerEnter (Collider other)
+	{
+		CharacterMovement mov = other.GetComponent<CharacterMovement>();
+		if (mov != null)
+		{
+			if (MoveBack == Type.MoveBackDown)
+				mov.MoveDown();
+			else if (MoveBack == Type.MoveBackUp)
+				mov.MoveUp();
+		}
+	}
+}
