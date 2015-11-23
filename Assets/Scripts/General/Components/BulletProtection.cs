53aecfbff898740186adad8b2b3b6038abbe0b98
rafael simao <raficher@gmail.com>
2015-11-23 15:09:25 -0200

    Created BulletProtection
    Created a script to avoid getting bullets from wrong sides(like
    friendly shots from enemies)

---
+++ Assets/Scripts/General/Components/BulletProtection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletProtection : MonoBehaviour
+{
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.GetComponent<Bullet>())
+			other.gameObject.SetActive(false);
+	}
+
+}
