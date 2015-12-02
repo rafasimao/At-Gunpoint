0619e6c08c5ee8e974c85cc83d9ccda856de2aec
rafael simao <raficher@gmail.com>
2015-12-02 11:43:16 -0200

    Bosses states applied

---
+++ Assets/Scripts/Controls/AIs/BossStates/FlyState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyState : BossState
+{
+
+	public enum FlyDirection {FlyUp, FlyDown}
+
+	public float FlySpeed;
+	public float Altitude;
+
+	public FlyDirection Direction;
+
+	Rigidbody _Rigidbody;
+	Vector3 _Movement;
+
+	public override void StateStart ()
+	{
+		//_Rigidbody = GetComponent<Rigidbody>();
+		//_Rigidbody.isKinematic = true;
+	}
+
+	public override bool StateUpdate ()
+	{
+		Vector3 dir = (Direction==FlyDirection.FlyUp) ? Vector3.up : Vector3.down;
+		_Movement = dir*FlySpeed*Time.deltaTime;
+		transform.Translate(_Movement);
+		//_Rigidbody.MovePosition(transform.position + _Movement);
+
+		bool result = (Direction==FlyDirection.FlyUp) ? 
+			(transform.position.y > Altitude) : (transform.position.y < Altitude);
+
+		//if (result)
+		//	_Rigidbody.isKinematic = false;
+
+		return result;
+	}
+}
