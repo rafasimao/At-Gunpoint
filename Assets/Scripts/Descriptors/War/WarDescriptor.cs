b008e1064217476a408b1ed282cd4c214a169a35
rafael simao <raficher@gmail.com>
2015-12-08 11:57:17 -0200

    Applied mission families to wars

--- Assets/Scripts/Descriptors/War/WarDescriptor.cs
+++ Assets/Scripts/Descriptors/War/WarDescriptor.cs
@@ -8,9 +8,9 @@ public class WarDescriptor : ScriptableObject
 		Run1 = 0, Run2 =1
 	}
 
-	public RunDescriptor Run1, Run2;
-
 	public MissionsFamilySetDescriptor MissionsFamily;
+
+	public RunDescriptor Run1, Run2;
 
 	public RunDescriptor GetRun (WarRun run)
 	{
