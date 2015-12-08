4bef222eb0888a296d9fcb6d03d32a1d18af09ad
rafael simao <raficher@gmail.com>
2015-12-07 22:04:31 -0200

    Created Coin Mission notification

--- Assets/Scripts/Elements/Collectables/Coin.cs
+++ Assets/Scripts/Elements/Collectables/Coin.cs
@@ -6,5 +6,6 @@ public class Coin : Collectable
 	protected override void BeCollected ()
 	{
 		GameController.Instance.GamePlayer.CollectCoins(Amount);
+		GameController.Instance.Missions.Notify(Mission.Actions.Collect,Mission.Objects.Coin);
 	}
 }
