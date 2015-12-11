1b1e31c36596229d4edc313ea7818cb9f0cab0d8
rafael simao <raficher@gmail.com>
2015-12-11 16:26:50 -0200

    Created PoitEffectView

---
+++ Assets/Scripts/Views/PointEffectView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PointEffectView : MonoBehaviour 
+{
+
+	public Text PointText;
+
+	public int Points;
+	public float Speed, FadeTime;
+	public Vector3 WorldPosition;
+
+	void OnEnable ()
+	{
+		PointText.text = "+"+Points;
+		transform.position = Camera.main.WorldToScreenPoint(WorldPosition);
+
+		Invoke("Disappear",FadeTime);
+	}
+
+	void Update ()
+	{
+		transform.Translate(Vector3.up * Speed * Time.deltaTime);
+	}
+
+	void Disappear ()
+	{
+		gameObject.SetActive(false);
+	}
+
+}
