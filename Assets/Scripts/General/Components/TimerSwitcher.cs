b34500a653373bbaf9ef937521d5e89e8d43cbce
rafael simao <raficher@gmail.com>
2015-12-14 10:03:58 -0200

    TimerSwitcher

---
+++ Assets/Scripts/General/Components/TimerSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerSwitcher : MonoBehaviour 
+{
+
+	public float FadeTime;
+	public GameObject SwitchOn;
+
+	void OnEnable ()
+	{
+		Invoke ("Disappear", FadeTime);
+	}
+
+	void Disappear ()
+	{
+		gameObject.SetActive(false);
+		if (SwitchOn!=null)
+			SwitchOn.SetActive(true);
+	}
+
+}
