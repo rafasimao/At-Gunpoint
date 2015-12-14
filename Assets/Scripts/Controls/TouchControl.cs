fa72cf61d5c8811ca3f27e1b8303d08d9ea386bc
rafael simao <raficher@gmail.com>
2015-12-14 20:34:00 -0200

    Created TouchControl

---
+++ Assets/Scripts/Controls/TouchControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class TouchControl : Control 
+{
+
+	protected override void OnStart ()
+	{}
+	
+	protected override void UpdateInputs ()
+	{
+		if (Input.touches.Length > 1)
+		{
+			if (!EventSystem.current.IsPointerOverGameObject(0))
+			{
+				Touch t = Input.GetTouch(0);
+
+				if (t.phase == TouchPhase.Began)
+				{
+					if (t.position.x < Screen.width/2)
+						Shooter.Fire(Vector3.left);
+					else if (t.position.y < Screen.height/2)
+						Movement.MoveDown();
+					else
+						Movement.MoveUp();
+				}
+			}
+		}
+	}
+
+}
