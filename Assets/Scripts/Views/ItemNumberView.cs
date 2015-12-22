ce7bdc7487f9bcb22fc9a716a5bbd578f225d9a0
rafael simao <raficher@gmail.com>
2015-12-22 11:57:58 -0200

    Created ItemNumberView

---
+++ Assets/Scripts/Views/ItemNumberView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ItemNumberView : MonoBehaviour 
+{
+	public Items.Item ItemType;
+
+	public Text ItemNumberText;
+
+	void OnEnable ()
+	{
+		UpdateView();
+	}
+
+	public void UpdateView ()
+	{
+		ItemNumberText.text = ""+GameController.Instance.GamePlayer.GetNumberOfOwnItems(ItemType);
+	}
+
+}
