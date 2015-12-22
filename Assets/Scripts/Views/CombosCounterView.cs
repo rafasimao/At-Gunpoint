2481801a748cc341071b22307cf5839898339057
rafael simao <raficher@gmail.com>
2015-12-22 12:32:21 -0200

    Added CombosCounter image

--- Assets/Scripts/Views/CombosCounterView.cs
+++ Assets/Scripts/Views/CombosCounterView.cs
@@ -6,10 +6,12 @@ public class CombosCounterView : MonoBehaviour
 {
 	public PlayerTracer Tracer;
 
+	public Image ComboImage;
 	public Text ComboText;
 
 	void LateUpdate () 
 	{
+		ComboImage.enabled = (Tracer.Combo>0);
 		ComboText.text = (Tracer.Combo>0) ? ""+Tracer.Combo : "";
 	}
 }
