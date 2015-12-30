3bf51110d6fc3cd670c9b83035beab5624403c54
rafael simao <raficher@gmail.com>
2015-12-30 01:02:04 -0200

    Applied languages to MissionView

--- Assets/Scripts/Views/MissionView.cs
+++ Assets/Scripts/Views/MissionView.cs
@@ -22,17 +22,19 @@ public class MissionView : MonoBehaviour
 			MissionText.text = 
 				GetQuestAction(mission.NeededAction)+" "+mission.Quantity+" ";
 			if (mission.NeededAction==Mission.Actions.Run)
-				MissionText.text += "METERS ";
+				MissionText.text += Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Meters);
 			MissionText.text += GetQuestObject(mission.NeededObject);
 
 			if (mission.IsGunNeeded)
-				MissionText.text += " USING A "+GetQuestGun(mission.NeededGun);
+				MissionText.text += Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_UsingA) + 
+					GetQuestGun(mission.NeededGun);
 
 			if (mission.IsSingleRun)
-				MissionText.text += " IN A SINGLE RUN";
+				MissionText.text += Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_SingleRun);
 
 			if (mission.Counter > 0 && !mission.IsCompleted)
-				MissionText.text += ". "+(mission.Quantity-mission.Counter)+" TO GO.";
+				MissionText.text += ". "+(mission.Quantity-mission.Counter) +
+					Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_ToGo);
 		}
 	}
 
@@ -42,34 +44,34 @@ public class MissionView : MonoBehaviour
 		switch (action)
 		{
 		case Mission.Actions.Kill:
-			result = "KILL";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Kill);
 			break;
 		case Mission.Actions.Destroy:
-			result = "DESTROY";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Destroy);
 			break;
 		case Mission.Actions.Explode:
-			result = "EXPLODE";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Explode);
 			break;
 		case Mission.Actions.IndirectAtack:
-			result = "INDIRECT ATTACK";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_IndirectAttack);
 			break;
 		case Mission.Actions.Collect:
-			result = "COLLECT";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Collect);
 			break;
 		case Mission.Actions.Trigger:
-			result = "TRIGGER";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Trigger);
 			break;
 		case Mission.Actions.Run:
-			result = "RUN";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Run);
 			break;
 		case Mission.Actions.GetAtZone:
-			result = "GET AT ZONE";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_GetAtZone);
 			break;
 		case Mission.Actions.Pass:
-			result = "PASS THROUGH";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_PassThrough);
 			break;
 		case Mission.Actions.Get:
-			result = "GET";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Get);
 			break;
 		}
 
@@ -85,49 +87,49 @@ public class MissionView : MonoBehaviour
 			result = "";
 			break;
 		case Mission.Objects.Enemy:
-			result = "ENEMIES";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Enemies);
 			break;
 		case Mission.Objects.Boss:
-			result = "BOSS";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.Boss);
 			break;
 		case Mission.Objects.BossNoDamage:
-			result = "BOSS WITHOUT TAKING ANY DAMAGE";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_BossNoDamage);
 			break;
 		case Mission.Objects.Target:
-			result = "TARGETS";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Targets);
 			break;
 		case Mission.Objects.Barrel:
-			result = "BARRELS";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Barrels);
 			break;
 		case Mission.Objects.Crate:
-			result = "CRATES";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Crates);
 			break;
 		case Mission.Objects.Mine:
-			result = "MINES";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Mines);
 			break;
 		case Mission.Objects.Coin:
-			result = "COINS";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Coins);
 			break;
 		case Mission.Objects.noFire:
-			result = "WITHOUT FIRING";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_NoFire);
 			break;
 		case Mission.Objects.noDamage:
-			result = "WITHOUT TAKING ANY DAMAGE";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_NoDamage);
 			break;
 		case Mission.Objects.noCoin:
-			result = "WITHOUT COLLECTING ANY COINS";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_NoCoins);
 			break;
 		case Mission.Objects.Zone:
-			result = "ZONES";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Zones);
 			break;
 		case Mission.Objects.BazookaBullet:
-			result = "BAZOOKA SHOTS WHILE FLYING";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_BazookaShots);
 			break;
 		case Mission.Objects.Combo:
-			result = "COMBOS";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_Combos);
 			break;
 		case Mission.Objects.NearMiss:
-			result = "NEAR MISSES";
+			result = Languages.GetPhrase(LanguageDescriptor.PhraseKey.MD_NearMisses);
 			break;
 		}
 
