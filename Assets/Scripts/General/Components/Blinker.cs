52e63371517297161a04b5fb5fc9e8ac0d059991
rafael simao <raficher@gmail.com>
2016-08-15 10:47:39 -0300

    Created Blinker and applied to "tap to play" texts

---
+++ Assets/Scripts/General/Components/Blinker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinker : MonoBehaviour 
+{
+
+	public bool IsTimeLimited;
+	public float TotalTime, BlinkTime, BetweenBlinksTime;
+	public Behaviour TargetBehaviour;
+	public GameObject TargetGameObject;
+	//float _Timer;
+
+	void OnEnable ()
+	{
+		Invoke("Blink", BetweenBlinksTime);
+	}
+
+	void Blink ()
+	{
+		SetTargets(false);
+		Invoke("Unblink", BlinkTime);
+	}
+
+	void Unblink ()
+	{
+		SetTargets(true);
+		Invoke("Blink", BetweenBlinksTime);
+	}
+
+	void SetTargets (bool active)
+	{
+		if (TargetBehaviour != null) TargetBehaviour.enabled = active;
+		if (TargetGameObject != null) TargetGameObject.SetActive(active);
+	}
+
+}
